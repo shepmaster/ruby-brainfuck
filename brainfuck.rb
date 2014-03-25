cc06890817e15f34f21b131f91c8338eeae4049e
Jake Goulding <jake.goulding@gmail.com>
2014-03-25 12:08:26 -0400

    Passes hello world test

---
+++ brainfuck.rb
@@ -0,0 +1,52 @@
+memory_idx = 0
+ip = 0
+main_memory = Array.new(30000, 0)
+
+program = File.read("program.bf")
+
+while ip < program.size
+  cmd_char = program[ip]
+  ip += 1
+
+  case cmd_char
+  when ">"
+    memory_idx += 1
+  when "<"
+    memory_idx -= 1
+  when "+"
+    main_memory[memory_idx] += 1
+  when "-"
+    main_memory[memory_idx] -= 1
+  when "."
+    STDOUT.putc(main_memory[memory_idx])
+  when ","
+    main_memory[memory_idx] = STDIN.getc
+  when "["
+    if main_memory[memory_idx] == 0
+      count = 0
+      until (count == 0 && program[ip] == "]")
+        if program[ip] == "["
+          count += 1
+        elsif program[ip] == "]"
+          count -= 1
+        end
+        ip += 1
+      end
+      ip += 1
+    end
+  when "]"
+    if main_memory[memory_idx] != 0
+      count = 0
+      ip -= 2
+      until (count == 0 && program[ip] == "[")
+        if program[ip] == "]"
+          count += 1
+        elsif program[ip] == "["
+          count -= 1
+        end
+        ip -= 1
+      end
+      ip += 1
+    end
+  end
+end
